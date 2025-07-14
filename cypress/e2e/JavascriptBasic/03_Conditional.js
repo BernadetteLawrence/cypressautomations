e24b3a9e5584b2cdd93d00ecf8f0ad1ceed4b31e
Bernadette <your@email.com>
2025-07-14 08:34:35 -0600

    Added

---
+++ cypress/e2e/JavascriptBasic/03_Conditional.js
@@ -0,0 +1,13 @@
+//if cart value is more than 1000 then apply discount of 10% else no discount
+function cartvaluediscount(carttotal)
+{
+
+if (carttotal >1000)
+    
+    {
+        let discount=carttotal * 0.10
+
+        let totalafterdiscount= carttotal - discount;
+        console.log(totalafterdiscount is +totalafterdiscount); 
+}
+let browser = "chrome";
