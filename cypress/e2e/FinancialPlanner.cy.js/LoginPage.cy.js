5299a0dfafb7188b0bf7465469286a5f21e2a6c3
Bernadette Lawrence <lawrencebernadettejane@gmail.com>
2025-08-20 12:58:12 -0600

    Updated login Page

--- cypress/e2e/FinancialPlanner.cy.js/LoginPage.cy.js
+++ cypress/e2e/FinancialPlanner.cy.js/LoginPage.cy.js
@@ -1,27 +1,31 @@
 import Login from "../../PageObjects/FinancialPlannerPOM/LoginPage";
-describe("Login POM", ()=> {
-    const login=new Login();
-   beforeEach(() => {
-    // Load the fixture file
- cy.fixture('FinancialPlanner').then((data) => { 
- cy.visit('https://publicplan.aivante.net/New_FP/Dzee/financial_planner/login')
- })
+describe("Login POM", () => {
+  let login;
+  let data;
+
+  before(() => {
+    cy.fixture("FinancialPlanner").then((d) => { data = d; });
+  });
+
+  beforeEach(() => {
+    login = new Login();  // ✅ assign to the outer variable
+    cy.visit("https://publicplan.aivante.net/New_FP/Dzee/financial_planner/login");
+  });
 
  it("TC_FP_Login_01: Verify Aivante logo is visible", () => {
  login.setAivanteLogo()
  })
     it("TC_FP_Login_02: Verify email input field", () => {
- login.setEMailUserID("data.UserID")
+ login.setEMailUserID(data.UserID)
     })
  it("TC_FP_Login_03: Verify password input field", () => {    
- login.setPassword("data.Password")
+ login.setPassword(data.Password)
  })
     it("TC_FP_Login_04: Verify login button is clickable", () => {
- login.clickLogin()
+    login.setEMailUserID(data.UserID);
+    login.setPassword(data.Password);
+    login.clickLogin();
+    login.verifyPlannerPage(data.FirstName);
     })
-    it("TC_FP_Login_05: Verify successful login", () => {
-        cy.url().should('include', '/Planner'); 
-    })
+   })
 
-    })
-})
