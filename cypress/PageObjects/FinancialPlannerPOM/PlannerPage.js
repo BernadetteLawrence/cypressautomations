228e29f660695ccfb1cd5406b1a1faf136e3e7c8
Bernadette Lawrence <lawrencebernadettejane@gmail.com>
2025-08-26 23:24:43 -0600

    updated pages

--- cypress/PageObjects/FinancialPlannerPOM/PlannerPage.js
+++ cypress/PageObjects/FinancialPlannerPOM/PlannerPage.js
@@ -1,15 +1,27 @@
-class Planner
-{
-    ClientEle='[id="gotoCreatePlan"]';
-    SearchEle='[id="plans_search"]';
-    CompareEle='[id="comparePlansIcon"]';
-    
+// cypress/PageObjects/FinancialPlannerPOM/PlannerPage.js
+class Planner {
+  constructor() {
+    this.clientEle  = '#gotoCreatePlan';
+    this.searchEle  = '#plans_search';
+    this.compareEle = '#comparePlansIcon';
+  }
 
+  // ✅ Wait for a reliable marker that proves the page is ready
+  waitForLoad() {
+    cy.get(this.clientEle, { timeout: 20000 }).should('be.visible');
+  }
 
-    setClient(Client)
-{
-cy.get(this.ClientEle).click();
-}
+  clickClient() {
+    cy.get(this.clientEle).should('be.visible').click();
+  }
+
+  typeSearch(value) {
+    cy.get(this.searchEle).should('be.visible').clear().type(value);
+  }
+
+  clickCompare() {
+    cy.get(this.compareEle).should('be.visible').click();
+  }
 }
 
 export default Planner;
