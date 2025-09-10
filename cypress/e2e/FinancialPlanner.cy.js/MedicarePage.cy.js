2c9acf54d1f4750859adb1a37f1ed1802de71907
Bernadette Lawrence <lawrencebernadettejane@gmail.com>
2025-09-10 13:18:28 -0600

    medicare updated

--- cypress/e2e/FinancialPlanner.cy.js/MedicarePage.cy.js
+++ cypress/e2e/FinancialPlanner.cy.js/MedicarePage.cy.js
@@ -8,7 +8,7 @@ describe('Medicare', () => {
         const LoginPage = new Login();
         const PlannerPage = new Planner();
         const PersonalPage = new Personal();
-        //const Medicare = new Medicare();
+        //const MedicarePage = new Medicare();
 
         cy.visit('https://publicplan.aivante.net/New_FP/Dzee/financial_planner/planner');
         cy.fixture('FinancialPlanner').then((data) => {
@@ -58,25 +58,95 @@ describe('Medicare', () => {
         cy.visit('https://publicplan.aivante.net/New_FP/Dzee/financial_planner/planner#create_new_plan');
     });
      it('TC_FP_MEDI_01', () => {                //landing on medicareTab ;
-       
-        Medicare.landingmedicaretab();
-        Medicare.clickmedicaretab();
+       const MedicarePage= new Medicare();
+        MedicarePage.clickmedicaretab();
 })
-it('TC_FP_MEDI_02', () => {                //tax filing label display
-    Medicare.landingmedicaretab(); 
-     Medicare.clickmedicaretab();   
-    Medicare.CheckTaxFilingPrimary()
+it('TC_FP_MEDI_02', () => {   
+     const MedicarePage= new Medicare();             //tax filing label display
+    MedicarePage.landingmedicaretab(); 
+    MedicarePage.clickmedicaretab();   
+    MedicarePage.CheckTaxFilingPrimaryStatus()
 
 
 })
  it('TC_FP_MEDI_03', () => {                //tax filing status
        //const MedicarePage = new Medicare();
       //  Medicare.personalclick();
-      Medicare.landingmedicaretab(); 
-      Medicare.clickmedicaretab();
-        Medicare.CheckTaxFilingPrimary();
-        Medicare.CheckTaxFilingPrimaryStatus();
+       const MedicarePage= new Medicare();
+       MedicarePage.landingmedicaretab(); 
+       MedicarePage.clickmedicaretab();
+       MedicarePage.CheckTaxFilingPrimary();
+       MedicarePage.CheckTaxFilingPrimaryStatus();
+ })
+ it('TC_FP_MEDI_04', () => {                //primary display
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.CheckPrimaryNameDisplay();
+        
+ })
+   it('TC_FP_MEDI_05', () => {                //primary name
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.checkPrimaryName();      
+
+ })
+ it('TC_FP_MEDI_06', () => {                //year and age
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.checkMedicareFromYearAndAge();
+       
+
+ })
+  it('TC_FP_MEDI_07', () => {                // year and life expectancy visible
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.checkMedicareFromYearAndAge();   
+
+ })
+ it('TC_FP_MEDI_08', () => {                //choose yaer and life expectancy
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.checkMedicareFromYearAndAge();
+        MedicarePage.selectMedicareToYearAndAge();
+ })
+  it('TC_FP_MEDI_09', () => {                //magi tier visible
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.verifymagitiername();
+        MedicarePage. magitieroptiondropdown();
+ })
+ it('TC_FP_MEDI_10', () => {                //magi tier dropdown
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.verifymagitiername();
+        MedicarePage. magitieroptiondropdown();
+        
+ })
+  it('TC_FP_MEDI_11', () => {                //length of magitier option
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.verifymagitiername();
+        MedicarePage. magitieroptiondropdown();
+        MedicarePage.LengthOfMagitierOption();
+ })
+   it('TC_FP_MEDI_12', () => {                //choose magi tier optiom
+        const MedicarePage = new Medicare();
+        MedicarePage.PersonalClick();
+        MedicarePage.clickmedicaretab();
+        MedicarePage.verifymagitiername();
+        MedicarePage. magitieroptiondropdown();
+        MedicarePage.LengthOfMagitierOption();
+        MedicarePage.chooseMagitierOption();
  })
 });
+
 
 
